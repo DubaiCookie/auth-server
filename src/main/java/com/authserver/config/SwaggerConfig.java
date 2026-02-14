aea7cdbe28f7d3a68f019552b52e592bbdcd4723
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2026-02-14 17:04:22 +0900

    fix: 스웨거 오류 수정

--- src/main/java/com/authserver/config/SwaggerConfig.java
+++ src/main/java/com/authserver/config/SwaggerConfig.java
@@ -1,21 +1,19 @@
 package com.authserver.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-                                import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Value("${app.server-url:}")
-    private String serverUrl;
+    @Value("${app.server-url}")
+    private String serverOrigin;
 
     @Bean
     public OpenAPI authServerOpenAPI() {
@@ -23,11 +21,11 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Auth Server API")
                         .description("Spring Boot 기반 인증 서버 REST API 문서")
-                        .version("v1.0.0"));
-
-        if (!serverUrl.isEmpty()) {
-            openAPI.addServersItem(new Server().url(serverUrl));
-        }
+                        .version("v1.0.0"))
+                .servers(List.of(
+                        new Server().url("/").description("Current Server"),
+                        new Server().url(serverOrigin).description("Production Server")
+                ));
 
         return openAPI;
     }
